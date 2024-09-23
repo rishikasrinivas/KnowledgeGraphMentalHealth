e8d9d1ba34004756254addffba0df201d5ccecd3
Rishika <04rishikasrinivas@gmail.com>
2024-09-22 18:02:10 -0700

    commiting to pull'

--- src/.ipynb_checkpoints/constants-checkpoint.py
+++ src/.ipynb_checkpoints/constants-checkpoint.py
@@ -1,9 +1,10 @@
-API_KEY='' #KEY REMEMBER TO DELETE BEFORE PUSHING
+API_KEY= '' #KEY REMEMBER TO DELETE BEFORE PUSHING
 MODEL_TYPE= 'gpt-4-turbo-preview' #MODEL TYPE
 DOCS_DIR = "Docs/" #DIRECTORY OF PAPERS
 SKIP = 2 #NUMBER OF PAGES TO READ AT A TIME
-PROMPT = """
-    json: GET THE MAIN IDEA!! You are to extract important entities, their definitions, overarching ideas,  and their relationships with one another from this paper. Don't copy the sentence word for word and split it up. Do not take all random relationships. Identify relationships that can help clinicians with diagnosis. . Will/May need to look across sections. Refrain from forming relationships unless text explicitly highlights or suggests that.
+TEMPERATURE=2
+PROMPT =  """
+     You are to extract important entities, their definitions, overarching ideas,  and their relationships with one another from this paper. Don't copy the sentence word for word and split it up. Do not take all random relationships. Identify relationships that can help clinicians with diagnosis. . Will/May need to look across sections. Refrain from forming relationships unless text explicitly highlights or suggests that.
     
     Understand the sentence and determine the relationships between the subject and objects
     
@@ -11,36 +12,47 @@ PROMPT = """
     If the text is referring to all types of depression for that relationship, label it as "All Depression"
     The idea is that these relationships can be pieced together into a knowledge graph that a clinician can trace. Keep this in mind as you extract.
     
-    Do not use any relationships other than these: 
-    1. treatment:  IPT --> treatment for -> depression
-    2. symptom: sadness -> symptom of -> depression
-    3. example: medA --> example of -> antidepressant
-    4. not effective: medA -> not effective for --> depression
-    5. effective: medA -> effective for -> depression
-    6. more effective: medA -> more effective than -> medB
-    7. less effective: medA -> less effective than -> medB
-    7. as effective as : medA -> as effective as -> medB
-    8. side effects: sneezing --> side effect of -> medA
-    9. treatment:  meditate --> treatment to -> reduce depression
-    10. Same efficacy: medA -> Same efficacy as -> no meds
-    11. Recommended first-line treatment: TreatA --> Recommended first-line treatment for --> illnesses
-    12. High Efficiency: MedA -> High Efficiency for -> DiseaseA
-    13. Less commonly used: MedB -> Less commonly used for -> Depression
-    14. correlated: DiseaseA -> correlated with -> ProblemA
-    15. More common than ... in ... : DiseaseA -> More common than -> DiseaseB -> in -> younger patients
-    16. Concurrent comorbidity: Disease A -> Concurrent comorbidity -> IllnessC
-    17. results: DiseaseA -> results -> faster treatment in young people
-    18. predictor of: use of medA -> predictor of -> diseaseB
-    19. Associateed with: Disease A -> associted with -> diases B
-    20. definition: therapyA -> definition -> defA
-    21: Correlation: ilnnessA -> correlation -> ilnessB
-    22. Most common treatment: DiseaseA -> Most common treatment -> MedA
-    23. Second most common treatment: DiseaseA - Second most common treatment -> MedA
-    24. Use: MedA -> use -> decreasing symptomA or medA -> used on -> patients with problem B
-    25. Properties: medC -> properties -> reduces this symptom by inihibiting that neuron
-    26: form of: IllnessA -> form of -> CategroyA
-    26. goal: MedA -> goal -> reduce anger
-    27: requirements: treatmentA -> requirements -> patient under 23
+    Do not use any relationships other than these (Note I've given the relationships and an example) :
+        1. Associated with: 
+        2. Results:
+        3. Symptom: sadness -> symptom of -> depression
+        4. Definition: axious depression -> definition -> major depressive disorder with high levels of anxiety symptoms 
+        5. Correlation: 
+
+
+        6. More common than … in …: Anxious depression --> more common than -> nonanxious depression -> in -> African American group, Hispanics, Primary Care Patients, Unemployed, Married then Divorced/Widowed, Less, Education, Public insurance, Lower-income
+
+
+        7. Co-occuring with:
+        8. Side effects:
+        9. Goal:  TreatmentA -> goal -> Bs
+        10. Treats: MedA --> treats -> B  OR TreatmentA --> treats -> B
+        11. Entails: TreatmentA -> entails -> B,C,D,E
+        12. Usage: A -> usage -> B
+        14. Comparable to: A -> Comparable to -> B
+        15. Properties: A -> properties -> B
+        16. Form of: A -> Form of -> B
+        17. Requirements: A -> requirements -> B
+        18. Overview:  A -> overivew ->  B
+        19. Benefit: A -> benefit -> B
+        20. Treatment options: A -> Treatment options -> B
+        21. Eligiblity: A -> eligibity -> B
+        22. Consistency: A -> consistency -> B
+        23. More effective: A -> more effective than -> medB OR treatA -> more effective for -> diseaseA
+        24. Less effective: medA -> less effective than -> medB OR treatA -> less effective for -> diseaseA
+        25. Same efficacy as ... for:  medA -> Same efficacy as -> medB OR treatA -> Same efficacy as -> treatB -> for -> diseaseA
+        27. Specific efficacy in treating: medA -> Specific efficacy in treating -> atypical features, such as reactive moods, reverse neuro-vegetative symptoms, and sensitivity to rejection
+        28. Highly effective: medA ->  Highly effective for -> diseaseB 
+        29. Moderately effective: medA ->  Highly effective for -> diseaseB 
+        30. More commonly used: medA ->  more commonly used -> diseaseB 
+        31. Less commonly used: treatmentA ->  Less commonly used for -> diseaseB 
+        32. Widely used: medA ->  widely used for -> diseaseB 
+        33. example: medA --> example of -> antidepressant
+        34. synonym: A -> synonym -> B
+        35. not effective: medA -> not effective for -> diseaseB
+    'synonym' should be frequently used to define concepts in the articles. Dont put subjects that are acronyms (IPT, ipt, etc) 
+    make sure to use 'synonym' 
+
     
     
     THESE SHOULD NOT BE THE SUBJ OR OBJ!!
@@ -49,30 +61,36 @@ PROMPT = """
 
     Examples: "bupropion is more likely than some ssris to lead to minimal weight gain or even weight loss."
         subj: bupropion 
-        rel: side effects 
-        obj: more likely than some ssris to lead to minimal weight gain or even weight loss minimal weight gain or even weight loss
+        rel:  side effects 
+        obj:  more likely than some ssris to lead to minimal weight gain or even weight loss minimal weight gain or even weight loss
     Examples: "venlafaxine's was seen to follow with less mental clarity than other medicines"
         subj: venlafaxine
-        rel: side effect
-        obj: less mental clarity
+        rel:  side effect
+        obj:  less mental clarity
 
 
 
     Example:  As shown in Table 2, remission rates were significantly lower in patients with anxious depression,
     according to both the HAM-D criterion (22.2% versus 33.4%) and the QIDS-SR criterion (27.5% versus 38.9%). 
     Response rates were also significantly lower for patients with anxious depression (41.7% versus 52.8%). "
+
+    Gives the rel(s): Anxious Depression ->  side effects -> low remission rates  and Anxious Depression -> side effects -> low response rates 
+
+    Develop relationships that all connect to depression. Meaning as subj, have A FORM OF DEPRESSION (following the 'depression' rules mentioned) as the subject, then a relation that connects depression to the object
+
     
-    Gives the rel(s): Anxious Depression -> finding -> low remission rates  and Anxious Depression -> finding -> low response rates 
+    Develop a dataframe that should have 4 headers, subj, rel, obj, ref. Reference should hold the exact sentence(s) (in closed quotes) that you used to get the relationship, This dataframe should explain the relationships between all subjects.
+
+    Return the dataframe as a list of dictionaries in the structure of
+    [
+        dictionaries
+    ]
+
+
     
-    Present 1 dataframe in an easily readable and visually appealing format.
-    The dataframe should explain the relationships between all subjects.
-    It should have 4 headers, subj, rel, obj, reference. Reference should hold the exact sentence(s) (in quotes) that you used to get the relationship
-    If you have object2 as a header, you must have a non-empty rel2 as a header. If thats not possible place the relationship in the next row
     
     
-    Return a pandas dataframe structure called paper1
-    Return a form of a dictionary of lists.
-    Avoid entities and relationships that are more than a few words. If you need to list multiple drug names or treatments, list them in different rows
     
     
     """
+ 
--- src/model.py
+++ src/model.py
@@ -4,51 +4,62 @@ import constants
 import data
 import os
 
-def get_response(client, src_text : str, prompt_llm :str, model_type: str):
-    prompt=f"Using this information: {src_text}, answer the following question: {prompt_llm}"
-
-    chat_completion=client.chat.completions.create(
-        temperature = 0.2,
-        messages=[
-            {
-                "role": "system",
-                "content": prompt_llm,
-                },
-            {
-                "role": "user",
-                "content": prompt,
-                }
-            ],
-        model= model_type,
-    )
-    resp=chat_completion.choices[0].message.content
-    return resp
+def get_response(client, src_text : list, prompt_llm :str, model_type: str):
+    text = [txt for txt in src_text]
+    final_resp = pd.DataFrame()
+    for text in src_text:
+        prompt=f'Using this information: {text}, answer the following question: {prompt_llm}'
+    
+        chat_completion=client.chat.completions.create(
+            temperature = 1,
+            messages=[
+                {
+                    "role": "system",
+                    "content": prompt_llm,
+                    },
+                {
+                    "role": "user",
+                    "content": prompt,
+                    }
+                ],
+            model= model_type,
+        )
+        final_resp = pd.concat([final_resp, parse_response(chat_completion.choices[0].message.content)])
+    return final_resp
 
 def parse_response(response) -> pd.DataFrame:
     df =pd.DataFrame()
-    
-    start_idx=response.find("data = ")
-    
-    if response[start_idx+7] == '[':
-        end_idx=response[start_idx:].find(']')
-    else:
-        end_idx=response[start_idx:].find('}')
+    start_idx=response.find('[')
+    print(response)
+    end_idx=response[start_idx:].find(']')
+    while response[start_idx + end_idx-2] != '}':
         
-    response=response[start_idx+7:start_idx+end_idx+1]
-    
+      
+        old=end_idx
+        end_idx=response[start_idx+end_idx+1:].find(']')+old+1
+                                                    
+        
+
+      
+    response=response[start_idx:start_idx+end_idx+2]
+
     df=pd.concat([df, pd.DataFrame(eval(response))])
-    df=df.drop_duplicates(subset=['subj', 'obj', 'rel'], keep='first')
+    df=df.drop_duplicates(subset=['subj', 'rel', 'obj'], keep='first').reset_index().drop(columns= ['index'])
+
     return df
 
 def main():
     client= OpenAI( api_key= constants.API_KEY)
-    file_text = ''
+    file_text = []
+    dfs=[]
     for documents in os.listdir(constants.DOCS_DIR):
         text =  data.read_file_text(documents)
         if text:
-            file_text += data.read_file_text(documents)
-    response = get_response(client, file_text, constants.PROMPT, constants.MODEL_TYPE)
-    triplet_dataframe = parse_response(response)
-    print(triplet_dataframe)
+            print("Reading text from ", documents)
+            file_text.append(text)
+        
+        response = get_response(client, file_text, constants.PROMPT, constants.MODEL_TYPE) 
+        dfs.append(response)
+    pd.concat([df for df in dfs]).to_csv("Temperature1.csv")
 main()
     
